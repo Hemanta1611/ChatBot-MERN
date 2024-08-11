7e8c832554fbb6fda1d6e75ecaabc2894304cb0c
Hemanta <hemantabhoi16112003@gmail.com>
2024-08-11 16:58:04 +0530

    Setting Up Routes For Users and Chats

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import {config} from "dotenv";
+import morgan from "morgan";
+import appRouter from './routes/index.js';
 config();
 
 const app = express();
@@ -7,5 +9,9 @@ const app = express();
 // middlewares
 app.use(express.json());
 
+// remove this during production
+app.use(morgan("dev"));
+
+app.use("/api/v1", appRouter);
 
 export default app;
