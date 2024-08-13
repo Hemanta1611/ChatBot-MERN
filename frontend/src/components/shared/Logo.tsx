5b7efb9d0d640addc24a27797aa11e0318f1f5f9
Hemanta <hemantabhoi16112003@gmail.com>
2024-08-13 17:58:09 +0530

    Added Authentication Context Provider

--- frontend/src/components/shared/Logo.tsx
+++ frontend/src/components/shared/Logo.tsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 import { Typography } from "@mui/material";
 
 const Logo = () => {
-    return <div style={{display:"flex", marginRight:"auto", alignItems:"center", gap:"8px"}}>
+    return <div style={{display:"flex", marginRight:"auto", alignItems:"center", gap:"16px"}}>
         <Link to={"/"}>
             <img src="openai.png" alt="openai" width={"30px"} height={"30px"} className="image-inverted" />
         </Link>
