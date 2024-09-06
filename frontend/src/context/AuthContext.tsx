8a2275d08e01cb4fa4bac88e7e7af743f3de93c6
Hemanta <hemantabhoi16112003@gmail.com>
2024-09-07 02:44:51 +0530

    Created Signup UI

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,5 +1,5 @@
 import {createContext, useState, ReactNode, useEffect, useContext} from "react";
-import { checkAuthStatus, loginUser, userLogout } from "../helpers/api-communicator";
+import { checkAuthStatus, loginUser, signupUser, userLogout } from "../helpers/api-communicator";
 
 type User = {
     name: string;
@@ -39,7 +39,13 @@ export const AuthProvider = ({children} : {children:ReactNode}) => {
             setIsLoggedIn(true);
         }
     };
-    const signup = async(name: string, email:string, password: string) => {};
+    const signup = async(name: string, email:string, password: string) => {
+        const data = await signupUser(name, email, password);
+        if(data){
+            setUser({email: data.email, name: data.name});
+            setIsLoggedIn(true);
+        }
+    };
     const logout = async() => {
         await userLogout();
         setIsLoggedIn(false);
